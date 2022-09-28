435509fda0da5678720145395f8c94a1f29dd88c
Luis-Rivas <71407850+Luis-Rivas@users.noreply.github.com>
2022-09-27 21:54:51 -0600

    se cambia la bd a postgresql

--- Proyecto_G3_TOO115/settings.py
+++ Proyecto_G3_TOO115/settings.py
@@ -77,8 +77,19 @@ WSGI_APPLICATION = 'Proyecto_G3_TOO115.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'gestion_asociados',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgresql',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
